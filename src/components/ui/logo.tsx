f1c16357c4826e2e4d6c7a3142d616337b8804d9
yogyy <yogimr261@gmail.com>
2024-05-14 17:09:14 +0700

    feat: add logo component

---
+++ src/components/ui/logo.tsx
@@ -0,0 +1,10 @@
+export function Hybrid() {
+  return (
+    <a href="/" className="flex items-center gap-1.5">
+      <img src="/icon.svg" alt="hybrid logo" draggable={false} />
+      <span className="text-[28px] font-semibold leading-[120%] tracking-tight">
+        Hybrid
+      </span>
+    </a>
+  );
+}
