1686d2113a1c4cebaba36109f5079d9e69fdb6a5
yogyy <yogimr261@gmail.com>
2024-05-15 15:57:00 +0700

    fix: issues with ts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,4 @@
-import type { Config } from "tailwindcss";
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
@@ -14,4 +13,4 @@ export default {
     },
   },
   plugins: [],
-} satisfies Config;
+};
