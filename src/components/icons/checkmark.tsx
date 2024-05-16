eec20b8e2ae8395eb7c4305632c6cdb7de03bb0b
yogyy <yogimr261@gmail.com>
2024-05-16 21:19:23 +0700

    feat: add icons

---
+++ src/components/icons/checkmark.tsx
@@ -0,0 +1,19 @@
+export const Checkmark = () => {
+  return (
+    <svg
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M17.4115 5L8.24479 14.1667L4.07812 10"
+        stroke="white"
+        strokeWidth="3"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
---
+++ src/components/icons/chevron-right.tsx
@@ -0,0 +1,19 @@
+export const ChevronRight = (props: React.SVGProps<SVGSVGElement>) => {
+  return (
+    <svg
+      width="9"
+      height="14"
+      viewBox="0 0 9 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="M1.5 13L7.5 7L1.5 1"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
