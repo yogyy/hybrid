698ea39062e3ecfd5954e153fa31efa8f0cc92f1
yogyy <yogimr261@gmail.com>
2024-05-17 01:37:07 +0700

    chore: update logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,6 +1,6 @@
 export function Hybrid() {
   return (
-    <a href="/" className="flex items-center gap-1.5">
+    <a href="/" className="flex w-fit items-center gap-1.5">
       <img src="/icon.svg" alt="hybrid logo" draggable={false} />
       <span className="text-[28px] font-semibold leading-[120%] tracking-tight">
         Hybrid
