152eeb56bdca10917f554f1ef65e5b1dacede1a3
yogyy <yogimr261@gmail.com>
2024-05-24 19:15:51 +0700

    fix: move logo, fixing notfound logo

--- src/components/ui/logo.tsx
+++ src/components/ui/logo.tsx
@@ -1,7 +1,9 @@
+import Logo from "@/assets/logo.svg";
+
 export function Hybrid() {
   return (
     <a href="/" className="flex w-fit items-center gap-1.5">
-      <img src="/icon.svg" alt="hybrid logo" draggable={false} />
+      <img src={Logo} alt="hybrid logo" draggable={false} />
       <span className="text-[28px] font-semibold leading-[120%] tracking-tight">
         Hybrid
       </span>
